ef06cfb422821946309d3867556b8f5b88d3120c
alvarojunior02 <alvaroamcjunior8@gmail.com>
2020-10-17 10:51:07 -0400

    Add route List Orphanages

--- src/controllers/OphanagesController.ts
+++ src/controllers/OphanagesController.ts
@@ -3,6 +3,14 @@ import { getRepository } from 'typeorm';
 import Orphanage from '../models/Orphanage';
 
 export default {
+    async index(request: Request, response: Response) {
+        const orphanageRepository = getRepository(Orphanage);
+
+        const orphanages = await orphanageRepository.find();
+
+        return response.json(orphanages);
+    },
+
     async create(request: Request, response: Response) {
         const {
             name,
